9f7abf3b506865825fa432d588f0c39e3d6f8e60
Charline Boulie <boulie@amazon.fr>
2023-09-19 19:32:01 +0200

    Datamodel

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsontourlondon": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "@fontsource/inter";
 
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+
+import awsconfig from "./aws-exports";
+
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(awsconfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
